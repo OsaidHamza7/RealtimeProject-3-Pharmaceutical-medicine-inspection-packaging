f643deac1f49430f9c86771c4843c441d92e145c
Maisam Alaa <52468869+maisamjuma@users.noreply.github.com>
2024-05-27 15:10:17 +0300

    Update pill_production_line.c

--- pill_production_line.c
+++ pill_production_line.c
@@ -5,6 +5,10 @@
 // Function Prototypes
 void getInformation(char **argv);
 void init_signals_handlers();
+void employee(void *args);
+void createPillMedicines(Pill_Production_Line *pill_production_line);
+
+
 //***********************************************************************************
 
 int production_line_num;
@@ -25,6 +29,7 @@ Pill_Production_Line *pill_production_lines;
 
 char *shmptr_pill_production_lines;
 int sem_pill_production_lines;
+sem_t mutex;
 
 int main(int argc, char **argv)
 {
@@ -50,9 +55,30 @@ int main(int argc, char **argv)
     // get information from the arguments
     getInformation(argv);
 
+    // Todo1: create the liquid medicines
+
+    sem_init(&mutex, 0, 1);
+    // creats threads for employees
+    pthread_t employees[num_of_pill_production_lines];
+
+    int employee_id[num_employees];
+    // create thread for each employee
+    for (int i = 0; i < num_of_pill_production_lines; i++)
+    {
+        employee_id[i] = i;
+        pthread_create(&employees[i], NULL, (void *)employee, (void *)&employee_id[i]);
+    }
+
+    // wait for the threads to finish
+    for (int i = 0; i < num_of_pill_production_lines; i++)
+    {
+        pthread_join(employees[i], NULL);
+    }
+
     // while (1)
     // {
     // }
+    sem_destroy(&mutex);
     return 0;
 }
 
@@ -104,4 +130,57 @@ void init_signals_handlers()
         perror("Signal Error\n");
         exit(-1);
     }*/
+}
+void employee(void *args)
+{
+    int *emp_id = (int *)args;
+
+    printf("Employee %d: started working\n", *emp_id);
+
+    while (1)
+    {
+        sem_wait(&mutex);
+        // Todo2: code the inspects the medicine
+
+        printf("i'm %d doing the task one\n", *emp_id);
+        sleep(3);
+        sem_wait(&mutex);
+        // Todo3: code the packaging the medicine
+
+        printf("i'm %d done task 1\n", *emp_id);
+        sleep(2);
+    }
+}
+void createPillMedicines(Pill_Production_Line *pill_Production_Line)
+{
+    for (int i = 0; i  < pill_Production_Line->num_medicines; i++)
+    {
+        pill_Production_Line->pill_medicines[i].id = i + 1;
+        pill_Production_Line->pill_medicines[i].production_line_num = pill_Production_Line->num;
+        pill_Production_Line->pill_medicines[i].num_plastic_containers = get_random_number(range_of_plastic_containers[0], range_of_plastic_containers[1]);
+        for(int j=0; j<  pill_Production_Line->pill_medicines->num_plastic_containers; j++)
+        {
+            pill_Production_Line->pill_medicines->plastic_containers[j].id= j+1;
+            pill_Production_Line->pill_medicines->plastic_containers[j].num_pills= get_random_number(range_of_pills[0],range_of_pills[1]);
+            for(int k=0; k<  pill_Production_Line->pill_medicines->plastic_containers->pills; k++)
+            {
+                pill_Production_Line->pill_medicines->plastic_containers->pills[k].id = k+1;
+                pill_Production_Line->pill_medicines->plastic_containers->pills[k].color = get_random_number(range_color_pill[0], range_color_pill[1]);
+                pill_Production_Line->pill_medicines->plastic_containers->pills[k].size = get_random_number(range_size_pill[0],range_size_pill[1]);
+            }
+        }
+        //pill_Production_Line->pill_medicines[i].Expiry_date=;
+    }
+    // print the liquid medicines
+    for (int i = 0; i < pill_Production_Line->num_medicines; i++)
+    {
+        printf("Pill Medicine %d is created with id %d, production line num %d, num plastic containers %d", pill_Production_Line->pill_medicines[i].id, pill_Production_Line->pill_medicines[i].production_line_num, pill_Production_Line->pill_medicines[i].num_plastic_containers);
+        fflush(stdout);
+        for(int j=0; j<  pill_Production_Line->pill_medicines->num_plastic_containers; j++)
+        {
+            printf("Plastic container with id %d, number of bills %d", pill_Production_Line->pill_medicines->plastic_containers[j].id,pill_Production_Line->pill_medicines->plastic_containers[j].num_pills);
+        }
+    }
+    printf("=====================================================================\n");
+    fflush(stdout);
 }
